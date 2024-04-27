5516ce40b1021513090ab5980cd86e5a9e91f378
Miomit <movsesyan-03@inbox.ru>
2024-04-27 21:40:03 +0400

    added print Header

--- src/table.hpp
+++ src/table.hpp
@@ -23,7 +23,9 @@ struct TableHader
 
     TableHader() = default;
 	TableHader(std::string str, Type t, uint8_t s): type{t}, size{s} {
+		if (str.length() > sizeof(name)) throw "str.length() > sizeof(name)";
 		str.copy(name, sizeof(name));
+		name[str.length()] = '\0';
 	}
     static auto NUM(std::string name) -> TableHader {
     	return TableHader(name, INT, 4);
@@ -33,6 +35,17 @@ struct TableHader
     }
  	static auto TEXT(std::string name, uint8_t size) -> TableHader {
     	return TableHader(name, CHAR, size);
+    }
+
+    auto print() -> void {
+    	std::cout << name << " ";
+    	if (type == INT) {
+    		std::cout << "INT" << std::endl;
+    	} else {
+    		std::cout << "CHAR";
+    		if (size > 1) std::cout << "[" << int(size) << "]";
+    		std::cout << std::endl;
+    	}
     }
 };
 
@@ -44,6 +57,8 @@ public:
     _path{path},
     _column{column} {
     	 _list = std::make_unique<flist>(path, 1 + sizeof(TableHader) * _column.size());
+
+    	 for (auto& col : _column) col.print();
     };
     
 private:
