d945c7637a555a10291eb5b87e030141815cbb4c
Miomit <movsesyan-03@inbox.ru>
2024-04-27 21:39:16 +0400

    added test create and open

--- src/main.cpp
+++ src/main.cpp
@@ -2,4 +2,15 @@
 
 auto main() -> int {
     
+    table::create(
+        "/home/miomit/my.tsql",
+        std::vector<TableHader>{
+            TableHader::NUM("id"),
+            TableHader::TEXT("login", 50),
+            TableHader::TEXT("password", 50),
+        }
+    );
+
+
+    table::open("/home/miomit/my.tsql");
 }
