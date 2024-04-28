4a5f6e227f26b920f60875a1993771a9f1246bb6
Miomit <movsesyan-03@inbox.ru>
2024-04-28 16:57:31 +0400

    added insert test

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,23 @@
 
 auto main() -> int {
     
-    table::create(
+    /*table::create(
         "/home/miomit/my.tsql",
         std::vector<TableHader>{
             TableHader::NUM("id"),
             TableHader::TEXT("login", 50),
             TableHader::TEXT("password", 50),
         }
+    );*/
+
+
+    auto users = table::open("/home/miomit/my.tsql");
+
+    users.insert(
+        std::map<std::string, std::string> {
+            {"id", "12321"},
+            {"login", "yura"},
+            {"password", "0123456789"}
+        }
     );
-
-
-    table::open("/home/miomit/my.tsql");
 }
